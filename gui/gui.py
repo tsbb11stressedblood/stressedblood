ff2ff218b898c59893be6c619e358ef5df125607
Christoph <chrhe043@student.liu.se>
2015-10-05 15:27:01 +0200

    Updated gui to (soon) include ROI

--- gui/gui.py
+++ gui/gui.py
@@ -15,29 +15,29 @@ import tkMessageBox
 from openslide import *
 import os
 from PIL import ImageTk as itk
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 import cv2
 
 
 class GUI:
     def __init__(self, master):
 
-        #First we need a frame, which serves as the basic OS-specific window
+        # First we need a frame, which serves as the basic OS-specific window
         self.frame = Frame(master)
-        #Packing it means that we want to fit it snuggly to the master and to make it visible
-        self.frame.pack()
+        # Packing it means that we want to fit it snuggly to the master and to make it visible
+        self.frame.pack(fill=BOTH, expand=YES)
 
-        #Next, we want a button where we can load an image
+        # Next, we want a button where we can load an image
         self.load_button = Button(self.frame, text="Load image", command=self.load_image)
         self.load_button.pack()
 
     def load_image(self):
-        #Get the filepath
+        # Get the filepath
         filename = tkFileDialog.askopenfilename()
 
-        #Do some error checking to see if it is good or nah
+        # Do some error checking to see if it is good or nah
         if filename:
-            #Try to load NDPI-image
+            # Try to load NDPI-image
             path, file_extension = os.path.splitext(filename)
 
             if file_extension.lower() == ".ndpi":
@@ -47,27 +47,47 @@ class GUI:
                 level_dims = ndpi_file.level_dimensions
                 print(level_dims)
 
-                #Try and get a PIL image and save it as PNG on same path
-                rgba_im = ndpi_file.read_region((0, 0), 3, (2048, 1856))
-                #rgba_im.save(path + ".png", "PNG")
+                # Try and get a PIL image and save it as PNG on same path
+                self.rgba_im = ndpi_file.read_region((0, 0), 3, (2048, 1856))
+                self.im_copy = self.rgba_im.copy()
+                # rgba_im.save(path + ".png", "PNG")
 
-                #Load with matplotlib and show it
-                #img = cv2.imread(path + ".png")
-                #plt.figure().add_subplot(111).imshow(img)
-                #plt.title("Success! (?)")
-                #plt.show()
+                # Load with matplotlib and show it
+                # img = cv2.imread(path + ".png")
+                # plt.figure().add_subplot(111).imshow(img)
+                # plt.title("Success! (?)")
+                # plt.show()
 
-                #Use TKs PhotoImage to show the image (needed for selecting ROIs I believe)
-                im = itk.PhotoImage(rgba_im)
-                label1 = Label(self.frame, image=im)
-                label1.image = im
-                #label1.place(x=0, y=0)
-                label1.pack()
+                # Use TKs PhotoImage to show the image (needed for selecting ROIs)
+                #resized = rgba_im.resize((300, 300), Image.ANTIALIAS)
+                self.im = itk.PhotoImage(self.rgba_im)
+                self.label1 = Label(self.frame, image=self.im)
+                #self.label1.image = self.im
+                self.label1.pack(fill=BOTH, expand=YES)
+                self.label1.bind('<Configure>', self.resize_image)
+                self.label1.bind('<B1-Motion>', self.select_ROI)
         else:
             show_error("There was a problem loading the file.")
 
+    def select_ROI(self, event):
+        print("Mouse is at: " + str(event.x) + " " + str(event.y))
 
-#Very quick and dirty wrappers for displaying message popup windows
+        bbox = (event.x, event.y)
+
+        self.item = self.frame.create_rectangle(bbox, outline="yellow")
+
+    def resize_image(self, event):
+
+        new_width = event.width
+        new_height = event.height
+
+        self.rgba_im = self.im_copy.resize((new_width, new_height))
+
+        self.im = itk.PhotoImage(self.rgba_im)
+        self.label1.configure(image=self.im)
+
+
+# Very quick and dirty wrappers for displaying message popup windows
 def show_msg(message):
     tkMessageBox.showinfo("Message", message)
 

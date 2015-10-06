d3f58d1a288b8a4a30398d2abb4296ba8cf27e64
Marcus Fallqvist <marfa861@student.liu.se>
2015-10-06 12:50:07 +0200

    pep8 kommentarer

--- Segmentation/Segmentation.py
+++ Segmentation/Segmentation.py
@@ -1,3 +1,9 @@
+""" Segmentation.py: The initial segmentation of cv2-images. Each Cell will be stored with its shapedata in the list cell_list
+
+Author: Marcus Fallqvist and Abdai Ahmed
+Date: 2015-10-06
+"""
+
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -5,8 +11,7 @@ from CellClass import *
 
 # Segmentation function, call this one with an image or image region to run - NOT 100% DONE.
 def segmentation( img ):
-    # image load and conversion to gray
-
+    # image load and conversion to gray and then threshold it
     gray = cv2.cvtColor(img ,cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     #plt.figure(1)
@@ -43,17 +48,24 @@ def segmentation( img ):
     plt.figure()
     plt.imshow(img, interpolation='nearest')
 
-    # Find countours and fit ellipses to the cells - NEED TO ASSIGN OBJECT:ELLIPSES
-    dummy, contours, hierarchy = cv2.findContours(thresh, 1, 2)
+    # Find countours and fit ellipses to the cells
+    #dum = cv2.cvtColor(markers ,cv2.COLOR_BGR2GRAY)
+    #markers[markers == -1] = 1
+    print(type(thresh))
+    print(type(markers))
+    dummyimg, contours, hierarchy = cv2.findContours(thresh, 1, 2)
     cell_list = []
     ellipse_list = []
-    for cnt in contours:
-        #cnt = contours[i]
+    for i,cnt in enumerate(contours):
+        # Fit ellipses on marked objects in the thresh-image
         ellipse = cv2.fitEllipse(cnt)
         ellipse_list.append(ellipse)
+        #Cut out region
+        x,y,w,h = cv2.boundingRect(cnt)
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
         #assign that shit: stuff[1][0], stuff[1][1]
-        cell_list.append(CellClass( ellipse ))
-
+        cell_list.append(Cell( ellipse, x,y,w,h ))
+        print cell_list[i].label
     #plot that shit
     for _ellipse in ellipse_list:
       markers = cv2.ellipse(img,_ellipse,(0,255,0),2)
@@ -64,9 +76,12 @@ def segmentation( img ):
 imgpath = 'smallbloodsmear.jpg'
 img =  cv2.imread(imgpath)
 cell_list = segmentation(img)
-print cell_list[0].label
+
 #stuff[1][1]
 
 #((97.40660095214844, 176.10752868652344), (24.298473358154297, 43.718692779541016), 73.73028564453125)
+
+
+
 plt.show()
 print("DONE")

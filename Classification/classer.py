0874655f790a84ac968f698bb2f2d8e8b5685946
Armand <armmo193@student.liu.se>
2015-11-18 19:12:05 +0100

    Made a fist functional classifier

---
+++ Classification/classer.py
@@ -0,0 +1,77 @@
+import cv2
+import pylab
+from whitecell import *
+from Segmentation import rbc_seg
+import itertools
+from sklearn.decomposition import PCA
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets, metrics
+
+iris = datasets.load_iris()
+def reduce_dimension(features, keep):
+    '''
+    Row = sample
+    Column = features
+
+    :param features:
+    :param keep:
+    :return: reduced_features
+    '''
+
+    features_shape = np.shape(features)
+
+    if keep > features_shape[1]:
+        print 'Error, not enough features'
+    else:
+        pca = PCA(n_components = keep)
+        reduced_features = pca.fit_transform(features)
+
+        return reduced_features
+
+
+
+def training(training_features, training_labels):
+    '''
+    Train
+    row = sample
+    column = feature
+    Test
+    Vector with lables
+    :param training_features:
+    :param training_labels:
+    :return: trained_classifier
+    '''
+
+    #svc = svm.SVC(kernel='linear', C=C).fit(X, y)
+    #rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, y)
+    #poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
+    #lin_svc = svm.LinearSVC(C=C).fit(X, y)
+
+    C = 1.0 # SVM regularization parameter
+    trained_classifier = svm.SVC(kernel='poly', degree=3, C=C).fit(training_features, training_labels)
+
+    return trained_classifier
+
+def classify_data(features,trained_classifier):
+    '''
+    :param features:
+    :param trained_classifier:
+    :return: predicted_classes
+    '''
+    predicted_classes  = trained_classifier.predict(features)
+
+    return predicted_classes
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Classification/mainclassification.py
@@ -0,0 +1,52 @@
+import classer as cl
+import cv2
+import pylab
+from whitecell import *
+from Segmentation import rbc_seg
+import itertools
+from sklearn.decomposition import PCA
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets, metrics
+
+
+# import some data to play with
+iris = datasets.load_iris()
+#X = iris.data[:, :2]  # we only take the first two features. We could
+                     # avoid this ugly slicing by using a two-dim dataset
+X = cl.reduce_dimension(iris.data, 2)
+y = iris.target
+
+trainer = cl.training(X,y)
+prediction = cl.classify_data(X,trainer)
+
+confusion = metrics.confusion_matrix(y, prediction)
+
+print confusion
+
+
+# create a mesh to plot in
+h = .02  # step size in the mesh
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, m_max]x[y_min, y_max].
+
+Z = trainer.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.title('SVC with polynomial (degree 3) kernel')
+plt.show()

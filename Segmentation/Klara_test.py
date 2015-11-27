7653ce2eb8edce745d5c86fc5431437cf4092e3a
Klara <klawi021@student.liu.se>
2015-11-27 11:52:05 +0100

    Updated watershed

--- Segmentation/Klara_test.py
+++ Segmentation/Klara_test.py
@@ -10,12 +10,6 @@ def cell_watershed(img):
     """
     img = img[:,:,0:3]
     kernel = np.ones((3,3),np.uint8)
-    plt.figure("im")
-    plt.imshow(img)
-
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    plt.figure("HSV")
-    plt.imshow(img_hsv[:,:,2])
 
     # Mask the background to get the whole cells
     color = 10
@@ -23,54 +17,42 @@ def cell_watershed(img):
     bg_transform = np.array(bg_transform * 255, dtype=np.uint8)
     bg_img = bg_transform.copy()
     unknown_mask = bg_transform.copy()
-    unknown_mask[bg_img<=0.5*np.amax(bg_img)] = 0
-    unknown_mask[bg_img>0.5*np.amax(bg_img)] = 1
+    unknown_mask[bg_img<=0.45*np.amax(bg_img)] = 0
+    unknown_mask[bg_img>0.45*np.amax(bg_img)] = 1
 
     # Erode and close to remove trash
-    unknown_mask = cv2.erode(unknown_mask, kernel, iterations =1)
+    #unknown_mask = cv2.erode(unknown_mask, kernel, iterations =1)
     unknown_mask = cv2.morphologyEx(unknown_mask,cv2.MORPH_CLOSE,kernel, iterations = 2)
 
-    plt.figure("whole cell")
-    plt.imshow(unknown_mask)
     # Mask to get the nuclei
-    color = 8
-    gray = spacetransformer.im2c(img, color)
-    gray = np.array(gray * 255, dtype=np.uint8)
-    gray[gray<.35*np.amax(gray)] = -1
-
-    #Play with other channels in the future
-    #color = 4
-    #gray_2 = spacetransformer.im2c(img, color)
-    #gray_2 = np.array(gray_2 * 255, dtype=np.uint8)
-    #gray_2[gray_2>0.05*np.amax(gray_2)] = -1
+    color = 4
+    nuclei = spacetransformer.im2c(img, color)
+    nuclei = np.array(nuclei * 255, dtype=np.uint8)
+    nuclei_mask = nuclei.copy()
+    nuclei_mask[nuclei< 0.05*np.amax(nuclei)] = 1
+    nuclei_mask[nuclei>=0.05*np.amax(nuclei)] = 0
 
     # Dilate and close to fill the nuclei
-    gray = cv2.dilate(gray, kernel, iterations=1)
-    close = cv2.morphologyEx(gray,cv2.MORPH_CLOSE,kernel, iterations = 3)
-    close[close == 255] = 0
+    nuclei_mask = cv2.erode(nuclei_mask, kernel, iterations =1)
+    nuclei_mask = cv2.morphologyEx(nuclei_mask,cv2.MORPH_CLOSE,kernel, iterations = 2)
 
-    plt.figure("nuclei")
-    plt.imshow(close)
     # Create the markers for the nuclei
-    ret, markers_nuc = cv2.connectedComponents(close)
+    ret, markers_nuc = cv2.connectedComponents(nuclei_mask)
 
-    plt.figure("m")
-    plt.imshow(markers_nuc)
 
     # Add the markers for the nuclei with the mask for the whole cells
     markers = markers_nuc | unknown_mask
 
-    plt.figure("markers")
-    plt.imshow(markers_nuc)
+    # Create a background image to use in the watershed
+    background =img.copy()
+    background[:,:,0] = unknown_mask*255
+    background[:,:,1] = unknown_mask*255
+    background[:,:,2] = unknown_mask*255
 
     # Perform watershed and mark the boarders on the image
-    markers = cv2.watershed(img, markers)
+    markers = cv2.watershed(background, markers)
     img[markers == -1] = [255,0,0]
-
-    plt.figure("res")
-    plt.imshow(img)
-    plt.show()
 
     return img, ret, markers
 
-cell_watershed(np.load("segmentation_test.npy"))
+cell_watershed(np.load("simple_test.npy"))

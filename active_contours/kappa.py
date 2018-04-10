1cc752846832b9d5d3ca3a41bd04897265d7d41b
Klara <klawi021@student.liu.se>
2018-04-10 21:20:05 +0200

    added active contours

--- active_contours/kappa.py
+++ active_contours/kappa.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -45,4 +46,53 @@ def kappa(I):
     #fxfxfyfy = (fx * fx + fy * fy)
     #K = (fxx * fy * fy - 2 * fxy * fx * fy + fyy * fx * fx) / np.sqrt(fxfxfyfy*fxfxfyfy*fxfxfyfy)
     #KG = K * G;
+=======
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+def kappa(I):
+    ux, uy = np.gradient(I)
+    normDu = np.sqrt(ux*ux + uy*uy + 1e-10)
+
+    Nx = ux / normDu
+    Ny = uy / normDu
+    nxx, _ = np.gradient(Nx)
+    _, nyy = np.gradient(Ny)
+    k = nxx + nyy
+
+    return k/np.max(np.max(np.abs(k)))
+    #I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+    #P = np.pad(I, 1, 'constant')
+    #m,n = np.shape(I)
+    #print m
+    #print n
+    #fy = P[2:, 1:n+1] - P[1:m+1, 1:n+1]
+    #fy = I - np.roll(I, 1, axis=1)
+    #fx = P[1:m+1, 2:]-P[1:m+1, 0:n]
+    #fx = I - np.roll(I, 1, axis=0)
+    #fyy = P[2:, 1:n+1]+P[0:m, 1:n+1]-2 * I
+    #fxx = P[1:m+1, 2:]+P[1:m+1, 0:n]-2 * I
+    #test1 = P[2:, 2:]-P[0:m, 2:]
+    #test2 = P[2:, 0:n]-P[0:m, 0:n]
+    #fxy = 0.25 * (test1+test2)
+
+    #fy = P(3:end, 2:n + 1)-P(1:m, 2:n + 1);
+    #fx = P(2:m + 1, 3:end)-P(2:m + 1, 1:n);
+    #fyy = P(3:end, 2:n + 1)+P(1:m, 2:n + 1)-2 * I;
+    #fxx = P(2:m + 1, 3:end)+P(2:m + 1, 1:n)-2 * I;
+    #fxy = 0.25. * (P(3:end, 3:end)-P(1:m, 3:end)+P(3:end, 1:n)-P(1:m, 1:n));
+
+    #plt.figure('fy')
+    #plt.imshow(fy)
+    #plt.figure('fyy')
+    #plt.imshow(fyy)
+    #plt.show()
+
+
+    #G = np.sqrt(fx * fx + fy * fy)
+    #fxfxfyfy = (fx * fx + fy * fy)
+    #K = (fxx * fy * fy - 2 * fxy * fx * fy + fyy * fx * fx) / np.sqrt(fxfxfyfy*fxfxfyfy*fxfxfyfy)
+    #KG = K * G;
+>>>>>>> 0ea58f3213c1c9043f8e6ace82251a5410c1c428
     #return KG
--- active_contours/signed_dist.py
+++ active_contours/signed_dist.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -68,4 +69,76 @@ plt.figure()
 plt.imshow(masked_img, cmap='gray')
 plt.figure('mask outline')
 plt.imshow(cell_mask_outline)
+=======
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from preprocessing import perform_watershed, load_images_and_name_from_folder, filter_image,separate_image,remove_edges_from_nuclei,fill_foreground
+from get_training_data import cluster_image_two
+import math
+
+def dirac(t):
+    return 1/(math.pi * (1 + t*t))
+
+def H(t):
+    return (1/2.0)*(1+ (2/math.pi)*math.atan(t))
+
+img = np.zeros([64,64])
+
+cv2.circle(img, (31,31), 24, (1), -1)
+
+img_inv = -( (img == 0)*(-2) + 1)
+plt.figure()
+plt.imshow(-img_inv)
+plt.show()
+
+im2, contours, hierarchy = cv2.findContours(img.astype(np.uint8), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+img2 = np.zeros_like(img)
+
+img_contour = cv2.drawContours(img2, contours, -1, 255, 1)
+img2 = cv2.distanceTransform(img2.astype(np.uint8) - 255, cv2.DIST_L2, 5)
+plt.figure()
+plt.imshow(img2*img_inv)
+plt.figure()
+plt.imshow(img2)
+plt.figure()
+plt.imshow(img_contour)
+
+plt.show()
+
+img = cv2.imread('../ground_truth/17green (5).png')
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_filtered = filter_image(img)
+res, values = cluster_image_two(img_filtered)
+foreground, nuclei = separate_image(img_filtered, values)
+nuclei = remove_edges_from_nuclei(nuclei)
+foreground = fill_foreground(foreground)
+markers_watershed = perform_watershed(foreground.astype(np.uint8), nuclei.astype(np.uint8))
+img[markers_watershed == -1] = [255, 0, 0]
+cell_mask = np.zeros_like(img)
+cell_mask = (markers_watershed==5)*1.0
+
+cv2.imwrite("cellmask.png", cell_mask)
+
+cell_mask_outline, cell_mask_contours, hierarchy = cv2.findContours(cell_mask.astype(np.uint8), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(cell_mask_outline, cell_mask_contours, -1, 255, 1)
+
+masked_img = np.zeros_like(img)
+
+masked_img[:,:,0] = img[:,:,0] * cell_mask
+masked_img[:,:,1] = img[:,:,1] * cell_mask
+masked_img[:,:,2] = img[:,:,2] * cell_mask
+
+masked_img = cv2.cvtColor(masked_img, cv2.COLOR_RGB2GRAY)
+
+plt.figure('mask')
+plt.imshow(cell_mask)
+plt.figure()
+plt.imshow(masked_img, cmap='gray')
+plt.figure('mask outline')
+plt.imshow(cell_mask_outline)
+>>>>>>> 0ea58f3213c1c9043f8e6ace82251a5410c1c428
 plt.show()
